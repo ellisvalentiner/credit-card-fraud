d1588c94da8fbbf219ee2b731bd4a2b52cd97a6f
Ellis Valentiner <ellisvalentiner@gmail.com>
2017-02-18 17:24:34 -0500

    add tensorboard

--- src/model.py
+++ src/model.py
@@ -4,7 +4,7 @@ import numpy as np
 import pandas as pd
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, SReLU
-from keras.callbacks import BaseLogger, ModelCheckpoint, EarlyStopping, ReduceLROnPlateau
+from keras.callbacks import BaseLogger, ModelCheckpoint, EarlyStopping, ReduceLROnPlateau, TensorBoard
 from sklearn.preprocessing import StandardScaler
 from sklearn.utils import compute_class_weight
 from sklearn.model_selection import StratifiedKFold
@@ -45,6 +45,7 @@ for train, test in kfold.split(X, Y):
     checkpointer = ModelCheckpoint(filepath="tmp/weights.hdf5", verbose=1, save_best_only=True)
     earlystop = EarlyStopping(monitor='val_loss', min_delta=1e-4, patience=5, verbose=0, mode='auto')
     reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.2, patience=5, min_lr=0.001)
+    tensor_board = TensorBoard(log_dir='./logs', histogram_freq=0, write_graph=True, write_images=False)
     # Compile model
     metrics = ['binary_accuracy', 'fmeasure', 'precision', 'recall']
     model.compile(loss='binary_crossentropy', optimizer='adam', metrics=metrics)
@@ -56,7 +57,7 @@ for train, test in kfold.split(X, Y):
                         shuffle=True,
                         validation_data=(X[test], Y[test]),
                         class_weight=class_weights,
-                        callbacks=[baselogger, checkpointer, earlystop, reduce_lr])
+                        callbacks=[baselogger, checkpointer, earlystop, reduce_lr, tensor_board])
     # Evaluate the model
     scores = model.evaluate(X[test], Y[test], verbose=1)
     print("%s: %.2f%%" % (model.metrics_names[1], scores[1]*100))
